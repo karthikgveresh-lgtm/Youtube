2c707a9409a7634bd3c03d4d126e3149f40ff12a
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-15 19:16:09 +0530

    feat: initialize express app and establish database connection with mongoose

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import express from "express"
+
+const app = express()
+
+    (async () => {
+        try {
+            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+            console.log("MongoDb Connected")
+            app.on("Error", (error) => {
+                console.log("ERROR : ", error)
+                throw error
+            })
+        } catch (error) {
+            console.error("ERROR : ", error)
+            throw error
+        }
+
+    })()

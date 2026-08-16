ca540b242a1033c8610ace422b5eb42e2b6eab5e
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-16 17:30:28 +0530

    feat: initialize MongoDB connection logic with environment configuration in index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
-import express from "express"
 
-const app = express()
 
-    (async () => {
-        try {
-            await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-            console.log("MongoDb Connected")
-            app.on("Error", (error) => {
-                console.log("ERROR : ", error)
-                throw error
-            })
 
-            app.listen(process.env.PORT, () => {
-                console.log(`App is running on port ${process.env.PORT}`)
-            })
-        } catch (error) {
-            console.error("ERROR : ", error)
-            throw error
-        }
+// import express from "express"
+// const app = express()
 
-    })()
+//     (async () => {
+//         try {
+//             await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//             console.log("MongoDb Connected")
+//             app.on("Error", (error) => {
+//                 console.log("ERROR : ", error)
+//                 throw error
+//             })
+
+//             app.listen(process.env.PORT, () => {
+//                 console.log(`App is running on port ${process.env.PORT}`)
+//             })
+//         } catch (error) {
+//             console.error("ERROR : ", error)
+//             throw error
+//         }
+
+//     })()

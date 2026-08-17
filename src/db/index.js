5ee88e36a1ab8def73863cf741c16ecb164e5a88
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-17 09:51:05 +0530

    feat: implement MongoDB connection logic using mongoose

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants";
 
-// const connectDB = async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         console.log("MongoDb Connected");
-//     } catch (error) {
-//         console.log("MONGODB Connection Error", error);
-//         process.exit(1)
-//     }
-// }
-
 const connectDb = async () => {
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)

37fc30ee4fb415d7e2fa624156a4fbc8de07226b
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-18 22:16:21 +0530

    feat: implement custom ApiError class for standardized error handling

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,9 +2,23 @@ class ApiError extends Error {
     constructor(
         ststusCode,
         message = "Something went Wrong",
-        error = [],
+        errors = [],
         stack = ""
     ) {
         super(message)
+        this.ststusCode = ststusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = this.errors
+
+        if (stack) {
+            this.stack = stack
+        }
+        else {
+            Error.captureStackTrace(this, this.constructor)
+        }
     }
 }
+
+export { ApiError }

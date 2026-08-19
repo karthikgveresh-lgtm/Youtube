71e74a5dcae1dbbe43981d973bc7eb3933fa31ed
karthikgveresh-Igtm <karthikgveresh@gmail.com>
2026-08-19 15:24:13 +0530

    feat: create User mongoose model with password hashing middleware

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -58,7 +58,13 @@ const UserSchema = new mongoose.Schema(
     { timestamps: true }
 )
 
-UserSchema.
+UserSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) return next();
+
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
 
 
 export const User = mongoose.model("User", UserSchema)
